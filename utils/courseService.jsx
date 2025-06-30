feeee86bd2548e98d443e64c16d3f0db8e1bc629
Tien viet kieu <kieuviettien1@gmail.com>
2025-06-30 21:28:21 +0700

    udapte-api

--- utils/courseService.jsx
+++ utils/courseService.jsx
@@ -1,67 +1,67 @@
-const API_URL = 'http://localhost:4000/courses'
+const API_URL = 'https://q28s8f-8080.csb.app/courses'
 
-export const getCourse = async() => {
-    const res = await fetch(API_URL, {
-        method: 'GET',
-    })
+export const getCourse = async () => {
+  const res = await fetch(API_URL, {
+    method: 'GET',
+  })
 
-    if (!res.ok) {
-        throw new Error('Failed to add course')
-    }
+  if (!res.ok) {
+    throw new Error('Failed to add course')
+  }
 
-    return await res.json()
+  return await res.json()
 }
-export const getCourseByID = async(id) => {
-    const res = await fetch(`${API_URL}?id=${id}`, {
-        method: 'GET',
-    })
-    if (!res.ok) {
-        throw new Error('Failed to add course')
-    }
+export const getCourseByID = async (id) => {
+  const res = await fetch(`${API_URL}?id=${id}`, {
+    method: 'GET',
+  })
+  if (!res.ok) {
+    throw new Error('Failed to add course')
+  }
 
-    return await res.json()
+  return await res.json()
 }
-export const addCourse = async(course) => {
-    const newCourse = {
-        ...course,
-        id: String(Date.now() + 1),
+export const addCourse = async (course) => {
+  const newCourse = {
+    ...course,
+    id: String(Date.now() + 1),
 
-        duration: course.duration + ' hours',
-    }
+    duration: course.duration + ' hours',
+  }
 
-    const res = await fetch(API_URL, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(newCourse),
-    })
+  const res = await fetch(API_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newCourse),
+  })
 
-    if (!res.ok) {
-        throw new Error('Failed to add course')
-    }
+  if (!res.ok) {
+    throw new Error('Failed to add course')
+  }
 
-    return await res.json()
-}
-
-export const deleteCourse = async(id) => {
-    const res = await fetch(`${API_URL}/${id}`, {
-        method: 'DELETE',
-    })
-
-    if (!res.ok) {
-        throw new Error('Failed to delete course')
-    }
+  return await res.json()
 }
 
-export const updateCourse = async(id, updatedData) => {
-    const res = await fetch(`${API_URL}/${id}`, {
-        method: 'PATCH',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(updatedData),
-    })
+export const deleteCourse = async (id) => {
+  const res = await fetch(`${API_URL}/${id}`, {
+    method: 'DELETE',
+  })
 
-    if (!res.ok) {
-        throw new Error('Failed to update course')
-    }
+  if (!res.ok) {
+    throw new Error('Failed to delete course')
+  }
+}
 
-    return await res.json()
+export const updateCourse = async (id, updatedData) => {
+  const res = await fetch(`${API_URL}/${id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(updatedData),
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed to update course')
+  }
+
+  return await res.json()
 }
